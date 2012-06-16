cdff4baf18f69154a012ad5e79bd309ee43d6e79
Chris Abrams <mail@chrisabrams.com>
2012-06-16 15:28:58 -0700

    Folder by date is working, now to get each post in it's respective folder.
    Signed-off-by: Chris Abrams <mail@chrisabrams.com>

--- src/muffin/bake.js
+++ src/muffin/bake.js
@@ -9,10 +9,22 @@
 Muffin.bake = function(o) {
 	var _this    = this,
 		command = Muffin.path.markx + ' ',
+		content,
+		contentOutput,
+		contentPieces,
+		contentPreview,
 		data,
 		date,
+		datePieces,
+		mainContent = '',
+		sortDate,
 		split,
-		title;
+		title,
+		obj = {},
+		url = '',
+		urlFormat,
+		muffinConfig = require(Muffin.path.called + '/muffin.json'),
+		ymd, hms, year, month, day, hour, minute, second;
 	o = (o || {});
 
 	//If a specific path was not called, then initialize in the current path of the CLI
@@ -20,77 +32,157 @@ Muffin.bake = function(o) {
 	var outputPath  = (o.outputPath || Muffin.path.called);
 	var articlePath = outputPath + '/articles';
 
-	(function(){
-	  if (typeof Object.defineProperty === 'function'){
-	    try{Object.defineProperty(Array.prototype,'sortBy',{value:sb}); }catch(e){}
-	  }
-	  if (!Array.prototype.sortBy) Array.prototype.sortBy = sb;
+	//Remove stuff as it will be wrotten agains
+	wrench.rmdirSyncRecursive(Muffin.path.called + '/public/blog', true);
 
-	  function sb(f){
-	    for (var i=this.length;i;){
-	      var o = this[--i];
-	      this[i] = [].concat(f.call(o,o,i),o);
-	    }
-	    this.sort(function(a,b){
-	      for (var i=0,len=a.length;i<len;++i){
-	        if (a[i]!=b[i]) return a[i]<b[i]?-1:1;
-	      }
-	      return 0;
-	    });
-	    for (var i=this.length;i;){
-	      this[--i]=this[i][this[i].length-1];
-	    }
-	    return this;
-	  }
-	})();
+	//Put fresh /blog back
+	//console.log(Muffin.path.called + '/public/blog');
+	//fs.mkdirSync(Muffin.path.called + '/public/blog', 0775);
 
 	//Get all articles to be built
 	readDir(articlePath, function(dunno, found) {
+		//console.log(arguments); return;
 		var dirs = found.dirs,
 			dirsList = [],
 			results;
 
-		//Build out the directories to write the files
+		//Loop through directories and construct object for each directory; push to array for building
 		dirs.forEach(function(dir, key) {
+			//console.log("dir ", dir);
 			if(typeof dir == "string") {
-				//console.log(dir);return;
-				var obj = {}, ymd, hms, datePieces, year, month, day, hour, minute, second;
+				obj = {}; //Needs to be reset or will append the new object n + 1 times
+				content = fs.readFileSync(dir + '/content.md', 'utf8');
+				
+				//If there is an h2, get everything before it (it will stop at anything h2 or greater)
+				try {
+					contentPieces = content.split('##');
+					obj.contentPreview = contentPieces[0];
+				}
+				
+				//If there isn't an h2, then get all of the content
+				catch(e) {
+					obj.contentPreview = content;
+				}
+
 				data = require(dir + '/data.json');
 				datePieces = data.date.split(' ');
-				//console.log(datePieces); return;
+
 				ymd = datePieces[0].split('-');
+				year = ymd[0];
+				month = ymd[1];
+				day = ymd[2];
+
 				hms = datePieces[1].split(':');
 
 				//(Y, M, D, H, M, S)
-				obj.date = new Date(ymd[0], ymd[1], ymd[2], hms[0], hms[1], hms[2]);
+				obj.sortDate = new Date(year, month, day, hms[0], hms[1], hms[2]);
 
+				//Get the last part of the path, the article name
 				obj.title = dir.split(/[/ ]+/).pop();
 
-				return dirsList.push(obj);
+				/*
+				Respective directory date creation
+				This can be done here or after this for loop - it doesn't matter too much but
+				we have to know the Y M D pieces in order to generate the date in the url for
+				each article. I'm sure this isn't being done super effeciently.
+				*/
+
+				urlFormat = muffinConfig.urlformat.split('/');
+
+				var urlPath = Muffin.path.called + '/public/blog';
+
+				//Parse URL Format
+				urlFormat.forEach(function(segment, key) {
+					/*
+					BUG: for some reason the first key is blank so we skip it
+					*/
+//console.log(urlPath);
+					if(key != 0) {
+
+						if(segment == "Y") {
+							urlPath = urlPath + '/' + year;
+						}
+
+						if(segment == "M") {
+							urlPath = urlPath + '/' + month;
+						}
+
+						if(segment == "D") {
+							urlPath = urlPath + '/' + day;
+						}
+
+						if(segment == "T") {
+							urlPath += '/' + obj.title;
+							
+						}
+
+						/*//Title - this one is last which is why we check for it, we don't care about the rest
+						if(segment === "T") {
+							//Create the last directory, the title of the blog post
+							
+							//fs.mkdirSync(urlPath);
+							
+							return;
+							//console.log(urlPath);
+						}
+						
+						else {
+							switch(segment) {
+								case "Y":
+									
+
+								case "M":
+									
+
+								case "D":
+									
+
+								default:
+									return;//this does nothing apparently
+							}
+						}*/
+					}
+				});
+				console.log(urlPath);
+				wrench.mkdirSyncRecursive(urlPath, 0775);
+
+				dirsList.push(obj);
 			}
 		});
 
-		/*dirs = dirs.sort((function(index){
-		    return function(a, b){
-		         console.log(a[index] + " " + b[index]);
-		        return (a[index] === b[index] ? 0 : (a[index] < b[index] ? -1 : 1));
-		    };
-		})(0)); // 2 is the index*/
-		//console.log(dirsList);
-		results = dirsList.sortBy(function(o) {return o.date});
+		//Sort results in descending order
+		results = dirsList.sortBy(function(o) {return -o.sortDate});
 
-		console.log(results);return;
+		//Main content page
+		results.forEach(function(result) {
+			if(result != "undefined" && result.contentPreview != "undefined") {
+				mainContent += result.contentPreview;
+			}
+		});
 
-		//Get the name of each directory to build as an array
-		/*var articleTitles = dirs.map(function(dir) {
-			return dir.split(/[/ ]+/).pop();
-		});*/
-		//console.log(articles);
+		//Convert the markdown to HTML for DOM manipulation
+		contentOutput = markdown.toHTML(mainContent);
 
+		//Do all of the DOM parsing
+		jsdom.env({
+			html: contentOutput,
+			scripts: [
+				Muffin.path.lib + '/jquery-1.7.2.min.js'
+			]
+		}, function (err, window) {
+			var $ = window.jQuery;
 
+			//Make h1's link to respective blog post
+			$('h1').wrap('<a href="#" />');
+
+			var output = $('body').html();
+
+			fs.writeFileSync(Muffin.path.called + '/public/index.html', output, 'utf8');
+			console.log("main page created");
+		});
+
+		//console.log(results);return;
 	});
-
-	
 
 	//Write the files
 
