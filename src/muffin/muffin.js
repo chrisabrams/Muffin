c16773122939b8d99727905c6afa70516c4b0ccc
Chris Abrams <mail@chrisabrams.com>
2012-06-16 18:34:10 -0700

    Getting really messy but pages are being compiled, yay!
    Signed-off-by: Chris Abrams <mail@chrisabrams.com>

--- src/muffin/muffin.js
+++ src/muffin/muffin.js
@@ -1,6 +1,11 @@
 var exec     = require('child_process').exec,
-	Flow     = require('flow'),
+	Flow     = require(__dirname + '/../node_modules/flow/lib/flow')({
+		called : process.cwd(),
+		lib    : __dirname + '/../lib',
+		markx  : __dirname + '/../node_modules/markx/bin/markx'
+	}), //It doesn't find it normally - stranga
 	fs       = require('fs-extra'),
+	jade     = require('jade'),
 	jsdom    = require('jsdom'),
 	markdown = require('markdown').markdown,
 	path     = require('path'),
